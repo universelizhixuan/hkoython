838cebe72b01c7d398dd0e7d7eaae4a431142539
universelizhixuan <lizhixuan@dfcv.com.cn>
2021-06-04 17:41:43 +0800

    mod

--- tools/imseg.py
+++ tools/imseg.py
@@ -202,72 +202,69 @@ class ImSeg():
                 break
 
     # 初版，函数完整作为参考
-    def segmentation(self):
-        i = 0
-        while True:
-            filepath = self.hktool.snapshot_normal_q.get()
-            # 建立存储处理完图像的文件夹
-            folder_path = '{}\\{}'.format(self.root_path, filepath.split('\\')[-2])
-            if not os.path.exists(folder_path):
-                print('mkdir {}'.format(folder_path))
-                os.mkdir(folder_path)
-            # 图像文件路径定位
-            new_filepath = '{}\\{}\\{}'.format(self.root_path,filepath.split('\\')[-2],filepath.split('\\')[-1])
-
-            ori_img = cv2.imread(filepath).astype(np.float32)
-            height, width = ori_img.shape[:2]
-
-            with open(filepath,'rb') as f:
-                img = f.read()
-
-            # TODO:实在太TM慢了，平均45秒！
-            now = datetime.datetime.now()
-            try:
-                result = requests.post(self.host, params={'threshold': 0.1},data=img).json()['results']
-            except Exception as e:
-                result = list()
-                with open('D:\\error.txt','a+') as f:
-                    f.write('{}:{}\n'.format(datetime.datetime.now(),e))
-            print('耗时：{}'.format(datetime.datetime.now() - now))
-            # 有结果，能识别出来
-            if result:
-                try:
-                    # 轮廓识别标签只有一个
-                    img = result[0]['mask']
-                    rle_obj = {"counts": img, "size": [height, width]}
-                    mask = mask_util.decode(rle_obj)
-
-                    # TODO：需要确认是否需要再encode
-                    new_rle_obj = mask_util.encode(mask)
-                    # TODO：需要定义一个确定的颜色
-                    random_color = np.array([np.random.random() * 255.0, np.random.random() * 255.0, np.random.random() * 255.0])
-
-                    idx = np.nonzero(mask)
-                    alpha = 0.5
-                    ori_img[idx[0], idx[1], :] *= 1.0 - alpha
-                    ori_img[idx[0], idx[1], :] += alpha * random_color
-
-                    ori_img = ori_img.astype(np.uint8)
-                    cv2.imwrite(new_filepath,ori_img)
-
-                    self.del_file.append(filepath)
-                    self.del_file_new.append(new_filepath)
-                    self.display_q.put(new_filepath)
-
-                    i += 1
-                    if not i % 3600:
-                        for j in range(0,3600):
-                            if os.path.exists(self.del_file[j]):
-                                os.remove(self.del_file[j])
-                            if os.path.exists(self.del_file_new[j]):
-                                os.remove(self.del_file_new[j])
-                        self.del_file.clear()
-                        self.del_file_new.clear()
-                        i = 0
-                except Exception as e:
-                    with open('D:\\error.txt','a+') as f:
-                        f.write('{}:{}\n'.format(datetime.datetime.now(),e))
-            # 无结果，无法识别出驾驶室
-            else:
-                # TODO：是否直接采取措施关停？
-                print('no result')
+    # def segmentation(self):
+    #     i = 0
+    #     while True:
+    #         filepath = self.hktool.snapshot_normal_q.get()
+    #         # 建立存储处理完图像的文件夹
+    #         folder_path = '{}\\{}'.format(self.root_path, filepath.split('\\')[-2])
+    #         if not os.path.exists(folder_path):
+    #             print('mkdir {}'.format(folder_path))
+    #             os.mkdir(folder_path)
+    #         # 图像文件路径定位
+    #         new_filepath = '{}\\{}\\{}'.format(self.root_path,filepath.split('\\')[-2],filepath.split('\\')[-1])
+    #
+    #         ori_img = cv2.imread(filepath).astype(np.float32)
+    #         height, width = ori_img.shape[:2]
+    #
+    #         with open(filepath,'rb') as f:
+    #             img = f.read()
+    #
+    #         # TODO:实在太TM慢了，平均45秒！
+    #         now = datetime.datetime.now()
+    #         try:
+    #             result = requests.post(self.host, params={'threshold': 0.1},data=img).json()['results']
+    #         except Exception as e:
+    #             result = list()
+    #             with open('D:\\error.txt','a+') as f:
+    #                 f.write('{}:{}\n'.format(datetime.datetime.now(),e))
+    #         print('耗时：{}'.format(datetime.datetime.now() - now))
+    #         # 有结果，能识别出来
+    #         if result:
+    #             try:
+    #                 # 轮廓识别标签只有一个
+    #                 img = result[0]['mask']
+    #                 rle_obj = {"counts": img, "size": [height, width]}
+    #                 mask = mask_util.decode(rle_obj)
+          
+    #                 new_rle_obj = mask_util.encode(mask)
+    #                 random_color = np.array([np.random.random() * 255.0, np.random.random() * 255.0, np.random.random() * 255.0])
+    #
+    #                 idx = np.nonzero(mask)
+    #                 alpha = 0.5
+    #                 ori_img[idx[0], idx[1], :] *= 1.0 - alpha
+    #                 ori_img[idx[0], idx[1], :] += alpha * random_color
+    #
+    #                 ori_img = ori_img.astype(np.uint8)
+    #                 cv2.imwrite(new_filepath,ori_img)
+    #
+    #                 self.del_file.append(filepath)
+    #                 self.del_file_new.append(new_filepath)
+    #                 self.display_q.put(new_filepath)
+    #
+    #                 i += 1
+    #                 if not i % 3600:
+    #                     for j in range(0,3600):
+    #                         if os.path.exists(self.del_file[j]):
+    #                             os.remove(self.del_file[j])
+    #                         if os.path.exists(self.del_file_new[j]):
+    #                             os.remove(self.del_file_new[j])
+    #                     self.del_file.clear()
+    #                     self.del_file_new.clear()
+    #                     i = 0
+    #             except Exception as e:
+    #                 with open('D:\\error.txt','a+') as f:
+    #                     f.write('{}:{}\n'.format(datetime.datetime.now(),e))
+    #         # 无结果，无法识别出驾驶室
+    #         else:
+    #             print('no result')
